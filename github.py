fb3374680e18d61a21e5d7b7ef571df199614417
douglaswcastro <douglaswcastro@hotmail.com>
2019-08-28 00:52:13 -0300

    criação da verificação do readme e da linguagem

--- github.py
+++ github.py
@@ -7,30 +7,55 @@ import threading
 API_URL = "https://api.github.com"
 SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
 
-
 class GitHub:
 
     def __init__(self, token=None):
         self.token = token
 
-    def process_user_followings(self, user):
+    def process_user_followings(self, user, typesearch, search):
         following_list = self.get_following(user)
 
+        following_list_order = []
         for following_user in following_list:
-            t = threading.Thread(target=self.process_user_repositories, args=(following_user,))
-            t.start()
+            # t = threading.Thread(target=self.process_user_repositories, args=(following_user,))
+            # t.start()
+            self.process_user_repositories(following_user, search)
+            following_list_order.append(tuple(following_user, self.process_user_repositories(following_user, search)))
+        # while threading.active_count() != 1:
+        # pass
 
-        while threading.active_count() != 1:
-            pass
+        if typesearch == '#Quem':
+            print(search)
+            print(typesearch)
+            print(following_list_order.sort(key=lambda x: x[1])[0])
 
-    def process_user_repositories(self, user):
+        elif typesearch == '#Quais':
+            print(search)
+            print(typesearch)
+            for users in following_list_order.sort(key=lambda x: x[1]):
+                print(users)
+
+        else:
+            print('parametro de pesquisa fora do padrão, por favor informe a pesquisa novamente')
+
+    def process_user_repositories(self, user, search):
         repositories = self.get_repositories_by_user(user)
         print("Getting {user} repositories".format(user=user))
-
+        countrepository = 0
+        countreadme = 0
         for repository in repositories:
             repo_language = self.get_repository_languages(user, repository)
+            for language in repo_language:
+                if search.upper() == language.upper():
+                    countrepository += 1
+
             print("{user}/{repo} {lang}".format(user=user, repo=repository, lang=repo_language))
-            self.get_repository_readme(user, repository)
+            readme = self.get_repository_readme(user, repository)
+
+            if readme.str().upper().contains(search.upper):
+                countreadme += 1
+
+        return countrepository + countreadme
 
     def get_following(self, user):
         following_url = "{url}/users/{user}/following".format(url=API_URL, user=user)
@@ -65,13 +90,15 @@ class GitHub:
         self._check_rate_limit()
 
         response = requests.get(repo_url, headers=self._get_auth_header())
-
+        readme = ""
         if response.status_code != 404:
             json_response = json.loads(response.text)
             readme_url = json_response["download_url"]
-            self._download_readme(readme_url, user, repo)
+            readme = self._download_readme(readme_url, user, repo)
         else:
-            print("Doesn't have README")
+            readme = "Doesn't have README"
+        print(readme)
+        return readme
 
     def _download_readme(self, readme_url, user, repo):
         readme_folder = os.path.abspath(os.path.join(SCRIPT_PATH, os.pardir)) + "/HelpBoot/readmes"
@@ -79,9 +106,11 @@ class GitHub:
         self._check_rate_limit()
 
         response = requests.get(readme_url, headers=self._get_auth_header())
-        readme_filename = "{folder}/README+{user}+{repo}.md".format(folder=readme_folder, user=user, repo=repo)
-        with open(readme_filename, 'wb') as readme_file:
-            readme_file.write(response.content)
+        print(response.content)
+        #readme_filename = "{folder}/README+{user}+{repo}.md".format(folder=readme_folder, user=user, repo=repo)
+        #with open(readme_filename, 'wb') as readme_file:
+            #readme_file.write(response.content)
+        return response.content
 
     def _check_rate_limit(self):
         if self._remaining_rate_limit() == 0:
@@ -98,5 +127,4 @@ class GitHub:
     def _get_auth_header(self):
         if self.token is not None:
             return {'Authorization': 'token {token}'.format(token=self.token)}
-
         return {}
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ user = "douglaswcastro"
 repo = "DAD-Parking---Back"
 
 token = '752ad7fc02a60df65fb8f31a30ba2bdab58880f7'
-
+typesearch = "#Quem"
+search = "C#"
 github = GitHub(token)
-github.process_user_followings(user)
+github.process_user_followings(user, typesearch, search)
