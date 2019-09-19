59fffdf6a78ab5b3c2b2f10c708e8c0bd9f53ba3
douglaswcastro <douglaswcastro@hotmail.com>
2019-09-19 00:45:30 -0300

    adicionando request get

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from load_env import load_dotenv
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 def foo():
 	data = json.loads(request.data)
 	print: "New commit by: {}".format[data['commits'][0]['author']['name']]
