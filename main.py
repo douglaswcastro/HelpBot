c3a3cebdb83453286a09597ca8af035bd96b82b1
douglaswcastro <douglaswcastro@hotmail.com>
2019-09-19 19:31:51 -0300

    alteração main para receber a requisição

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
-from github import GitHub
-from flask import Flask, request
-import json
 import os
-from load_env import load_dotenv
-
+import json
+from flask import Flask, request
+from github import GitHub
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def foo():
 	data = json.loads(request.data)
-	print: "New commit by: {}".format[data['commits'][0]['author']['name']]
+	print: "New commit by: {}".format(data['commits'][0]['author']['name'])
 	return "OK"
+
+
+if __name__ == '__main__':
+	app.run()
 
 
 #user = "douglaswcastro"
 
-#token = os.getenv("TOKEN")
+token = os.getenv("TOKEN")
 #typesearch = "#Quais"
 #search = "Correios"
-
-
 #github = GitHub(token)
 #github.process_user_followings(user, typesearch, search)
