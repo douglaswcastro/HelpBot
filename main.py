af68b0495e6d29674f07b3a97c694b7c96e0d64d
douglaswcastro <douglaswcastro@hotmail.com>
2019-09-19 00:35:48 -0300

    criando endpoint

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from github import GitHub
+from flask import Flask, request
+import json
 import os
 from load_env import load_dotenv
 
-user = "douglaswcastro"
 
-token = os.getenv("TOKEN")
-typesearch = "#Quais"
-search = "Correios"
-github = GitHub(token)
-github.process_user_followings(user, typesearch, search)
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST'])
+def foo():
+	data = json.loads(request.data)
+	print: "New commit by: {}".format[data['commits'][0]['author']['name']]
+	return "OK"
+
+
+#user = "douglaswcastro"
+
+#token = os.getenv("TOKEN")
+#typesearch = "#Quais"
+#search = "Correios"
+
+
+#github = GitHub(token)
+#github.process_user_followings(user, typesearch, search)
