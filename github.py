9d335f0ed2315c1794ec1b3710f205529fe1f6c7
douglaswcastro <douglaswcastro@hotmail.com>
2019-09-26 00:24:12 -0300

    configuração requisição de respota robô

--- github.py
+++ github.py
@@ -1,3 +1,5 @@
+from _ctypes import get_last_error
+
 import requests
 import json
 import os
@@ -10,139 +12,145 @@ SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
 
 
 class GitHub:
-	def __init__(self, token=None):
-		self.token = token
+    def __init__(self, token=None):
+        self.token = token
 
-	def process_user_followings(self, user, typesearch, search):
-		following_list = self.get_following(user)
-		user_return = ""
-		following_list_order = []
-		for following_user in following_list[0:2]:
-			# t = threading.Thread(target=following_list_order.append({'user': following_user, 'value': self.process_user_repositories(following_user, search)}))
-			# t.start()
-			following_list_order.append(
-				{'user': following_user, 'value': self.process_user_repositories(following_user, search)})
-		# while threading.active_count() != 1:
-		# pass
+    def process_user_followings(self, user, typesearch, search):
+        following_list = self.get_following(user)
+        user_return = ""
+        following_list_order = []
+        for following_user in following_list[0:2]:
+            # t = threading.Thread(target=following_list_order.append({'user': following_user, 'value': self.process_user_repositories(following_user, search)}))
+            # t.start()
+            following_list_order.append(
+                {'user': following_user, 'value': self.process_user_repositories(following_user, search)})
+        # while threading.active_count() != 1:
+        # pass
 
-		if typesearch.upper() == '#QUEM':
-			print(search)
-			print(typesearch)
-			user_return = sorted(following_list_order, key=lambda x: x['value'], reverse=True)[0]['user']
-		# return user_return
-		# following_list_order.sort(reverse=True, key=myFunc)[1])
+        if typesearch.upper() == '#QUEM':
+            print(search)
+            print(typesearch)
+            user_return = sorted(following_list_order, key=lambda x: x['value'], reverse=True)[0]['user']
+        # return user_return
+        # following_list_order.sort(reverse=True, key=myFunc)[1])
 
-		elif typesearch.upper() == '#QUAIS':
-			print(search)
-			print(typesearch)
-			for users in sorted(following_list_order, key=lambda x: x['value'], reverse=True):
-				print(users['user'])
-			user_return = sorted(following_list_order, key=lambda x: x['value'], reverse=True)['user']
-		# return user_return
+        elif typesearch.upper() == '#QUAIS':
+            print(search)
+            print(typesearch)
+            for users in sorted(following_list_order, key=lambda x: x['value'], reverse=True):
+                print(users['user'])
+            user_return = sorted(following_list_order, key=lambda x: x['value'], reverse=True)['user']
+        # return user_return
 
-		else:
-			user_return = 'parametro de pesquisa fora do padrao, por favor informe a pesquisa novamente'
+        else:
+            user_return = 'parametro de pesquisa fora do padrao, por favor informe a pesquisa novamente'
 
-		return user_return
+        return user_return
 
-	def process_user_repositories(self, user, search):
-		repositories = self.get_repositories_by_user(user)
-		print("Getting {user} repositories".format(user=user))
-		countrepository = 0
-		countreadme = 0
-		for repository in repositories:
-			repo_language = self.get_repository_languages(user, repository)
-			for language in repo_language:
-				if search.upper() == language.upper():
-					countrepository += 1
+    def process_user_repositories(self, user, search):
+        repositories = self.get_repositories_by_user(user)
+        print("Getting {user} repositories".format(user=user))
+        countrepository = 0
+        countreadme = 0
+        for repository in repositories:
+            repo_language = self.get_repository_languages(user, repository)
+            for language in repo_language:
+                if search.upper() == language.upper():
+                    countrepository += 1
 
-			readme = self.get_repository_readme(user, repository)
+            readme = self.get_repository_readme(user, repository)
 
-			if readme != "Doesn't have README" and search.upper() in readme.upper():
-				countreadme += 1
-		# print("{user}/{repo} {lang} - {count}".format(user=user, repo=repository, lang=repo_language, count=(countrepository + countreadme)))
-		# print("{user} - {count}".format(user=user, count=(countrepository + countreadme)))
-		return countrepository + countreadme
+            if readme != "Doesn't have README" and search.upper() in readme.upper():
+                countreadme += 1
+        # print("{user}/{repo} {lang} - {count}".format(user=user, repo=repository, lang=repo_language, count=(countrepository + countreadme)))
+        # print("{user} - {count}".format(user=user, count=(countrepository + countreadme)))
+        return countrepository + countreadme
 
-	def get_following(self, user):
-		following_url = "{url}/users/{user}/following".format(url=API_URL, user=user)
+    def get_following(self, user):
+        following_url = "{url}/users/{user}/following".format(url=API_URL, user=user)
 
-		self._check_rate_limit()
+        self._check_rate_limit()
 
-		response = requests.get(following_url, headers=self._get_auth_header())
-		following_list = [user["login"] for user in json.loads(response.text)]
-		return following_list
+        response = requests.get(following_url, headers=self._get_auth_header())
+        following_list = [user["login"] for user in json.loads(response.text)]
+        return following_list
 
-	def get_repositories_by_user(self, user):
-		repos_url = "{url}/users/{user}/repos".format(url=API_URL, user=user)
+    def get_repositories_by_user(self, user):
+        repos_url = "{url}/users/{user}/repos".format(url=API_URL, user=user)
 
-		self._check_rate_limit()
+        self._check_rate_limit()
 
-		response = requests.get(repos_url, headers=self._get_auth_header())
-		repos_name = [repo["name"] for repo in json.loads(response.text)]
-		return repos_name
+        response = requests.get(repos_url, headers=self._get_auth_header())
+        repos_name = [repo["name"] for repo in json.loads(response.text)]
+        return repos_name
 
-	def get_repository_languages(self, user, repo):
-		repo_url = "{url}/repos/{user}/{repo}/languages".format(url=API_URL, user=user, repo=repo)
+    def get_repository_languages(self, user, repo):
+        repo_url = "{url}/repos/{user}/{repo}/languages".format(url=API_URL, user=user, repo=repo)
 
-		self._check_rate_limit()
+        self._check_rate_limit()
 
-		response = requests.get(repo_url, headers=self._get_auth_header())
-		repository_languages = [key for key in json.loads(response.text)]
-		return repository_languages
+        response = requests.get(repo_url, headers=self._get_auth_header())
+        repository_languages = [key for key in json.loads(response.text)]
+        return repository_languages
 
-	def get_repository_readme(self, user, repo):
-		repo_url = "{url}/repos/{user}/{repo}/readme".format(url=API_URL, user=user, repo=repo)
+    def get_repository_readme(self, user, repo):
+        repo_url = "{url}/repos/{user}/{repo}/readme".format(url=API_URL, user=user, repo=repo)
 
-		self._check_rate_limit()
+        self._check_rate_limit()
 
-		response = requests.get(repo_url, headers=self._get_auth_header())
-		readme = ""
-		if response.status_code != 404:
-			json_response = json.loads(response.text)
-			readme_url = json_response["download_url"]
-			readme = self._download_readme(readme_url, user, repo)
-		else:
-			readme = "Doesn't have README"
+        response = requests.get(repo_url, headers=self._get_auth_header())
+        readme = ""
+        if response.status_code != 404:
+            json_response = json.loads(response.text)
+            readme_url = json_response["download_url"]
+            readme = self._download_readme(readme_url, user, repo)
+        else:
+            readme = "Doesn't have README"
 
-		return readme
+        return readme
 
-	def _download_readme(self, readme_url, user, repo):
-		#readme_folder = os.path.abspath(os.path.join(SCRIPT_PATH, os.pardir)) + "/HelpBoot/readmes"
+    def _download_readme(self, readme_url, user, repo):
+        # readme_folder = os.path.abspath(os.path.join(SCRIPT_PATH, os.pardir)) + "/HelpBoot/readmes"
 
-		self._check_rate_limit()
+        self._check_rate_limit()
 
-		response = requests.get(readme_url, headers=self._get_auth_header())
-		# print(response.content)
-		# print(response.text)
-		# readme_filename = "{folder}/README+{user}+{repo}.md".format(folder=readme_folder, user=user, repo=repo)
-		# with open(readme_filename, 'wb') as readme_file:
-		# readme_file.write(response.content)
-		return response.text
+        response = requests.get(readme_url, headers=self._get_auth_header())
+        # print(response.content)
+        # print(response.text)
+        # readme_filename = "{folder}/README+{user}+{repo}.md".format(folder=readme_folder, user=user, repo=repo)
+        # with open(readme_filename, 'wb') as readme_file:
+        # readme_file.write(response.content)
+        return response.text
 
-	def _check_rate_limit(self):
-		if self._remaining_rate_limit() == 0:
-			print("Wait a minute")
-			time.sleep(5)
-			self._check_rate_limit()
+    def _check_rate_limit(self):
+        if self._remaining_rate_limit() == 0:
+            print("Wait a minute")
+            time.sleep(5)
+            self._check_rate_limit()
 
-	def _remaining_rate_limit(self):
-		rate_url = "{url}/rate_limit".format(url=API_URL)
-		response = requests.get(rate_url, headers=self._get_auth_header())
-		json_response = json.loads(response.text)
-		return json_response["rate"]["remaining"]
+    def _remaining_rate_limit(self):
+        rate_url = "{url}/rate_limit".format(url=API_URL)
+        response = requests.get(rate_url, headers=self._get_auth_header())
+        json_response = json.loads(response.text)
+        return json_response["rate"]["remaining"]
 
-	def _get_auth_header(self):
-		if self.token is not None:
-			return {'Authorization': 'token {token}'.format(token=self.token)}
-		return {}
+    def _get_auth_header(self):
+        if self.token is not None:
+            return {'Authorization': 'token {token}'.format(token=self.token)}
+        return {}
 
-	def get_last_commit_repo(self):
-		repos_url = "{url}/repos/{owner}/{repository_bot}/commits".format(url=API_URL, owner=owner, repository_bot=reposity_bot)
+    def get_last_commit_repo(self):
+        repos_url = "{url}/repos/{owner}/{repository_bot}/commits".format(url=API_URL, owner=owner,
+                                                                          repository_bot=reposity_bot)
 
-		self._check_rate_limit()
+        self._check_rate_limit()
 
-		response = requests.get(repos_url, headers=self._get_auth_header())
-		json_response = json.loads(response.text)
-		return json_response[0]["sha"]
+        response = requests.get(repos_url, headers=self._get_auth_header())
+        json_response = json.loads(response.text)
+        return json_response[0]["sha"]
 
+    def response_comment(self, response):
+        sha_comment = get_last_commit_repo()
+        payload = {'body': response}
+        requests.post("{url}/repos/{owner}/{repository_bot}/commits/{sha}/comment"
+                      .format(url=API_URL, owner=owner, repository_bot=reposity_bot, sha=sha_comment), data=payload)
