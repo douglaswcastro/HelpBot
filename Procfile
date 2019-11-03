e7ecb2725a92e0c9c7e2b7d5b3f2044358be7abe
douglaswcastro <douglaswcastro@hotmail.com>
2019-11-03 13:05:52 -0300

    configuração fila para processar os dados

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn main:app
+worker: python worker.py
--- main.py
+++ main.py
@@ -2,12 +2,15 @@ from flask import Flask, request
 from github import GitHub
 import os
 import json
+from rq import Queue
+from worker import conn
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['POST'])
 def foo():
+    q = Queue(connection=conn)
     print('inicio processamento')
     token = os.getenv("TOKEN")
     data = json.loads(request.data)
@@ -16,15 +19,16 @@ def foo():
     bot = comentary[1:8]
     typesearch = comentary[9:15].strip()
     search = comentary[14:].lstrip()
-    text_return = ""
-    if bot.upper() != "HELPBOT":
-        return "O Robô não foi citado no comentário, por favor cite o robô"
-    github = GitHub(token)
-    text_return = github.process_user_followings(user, typesearch, search)
+    result = ""
+    if bot.upper() == "HELPBOT":
+    # return "O Robô não foi citado no comentário, por favor cite o robô"
+        github = GitHub(token)
+        result = q.enqueue(github.process_user_followings(user, typesearch, search))
+    # text_return =
 
-    #github.response_comment(user, text_return)
+    # github.response_comment(user, text_return)
 
-    return text_return
+    return result
 
 
 if __name__ == '__main__':
